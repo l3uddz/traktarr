b3b5a9f9ec9edd6edc5477ca91c178b035c649cc
l3uddz <l3uddz@gmail.com>
2018-05-05 19:04:19 +0100

    authenticate traktarr help text change

--- traktarr.py
+++ traktarr.py
@@ -57,7 +57,7 @@ def app(config, logfile):
 # Trakt OAuth
 ############################################################
 
-@app.command(help='Authenticate Traktarr to index your personal lists')
+@app.command(help='Authenticate traktarr.')
 def trakt_authentication():
     from media.trakt import Trakt
     trakt = Trakt(cfg)
