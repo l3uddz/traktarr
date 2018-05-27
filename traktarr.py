d4b996a02db04834312b6f47f0758dec8676c78d
James <l3uddz@gmail.com>
2018-05-27 20:56:28 +0100

    increase version

--- traktarr.py
+++ traktarr.py
@@ -17,7 +17,7 @@ notify = None
 
 # Click
 @click.group(help='Add new shows & movies to Sonarr/Radarr from Trakt.')
-@click.version_option('1.2.0', prog_name='traktarr')
+@click.version_option('1.2.1', prog_name='traktarr')
 @click.option(
     '--config',
     envvar='TRAKTARR_CONFIG',
