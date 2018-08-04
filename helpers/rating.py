29fa7c39209bb2c50ba8b535e342f920a772e00d
davidaghaian@student.cccd.edu <daghaian@student.cccd.edu>
2018-08-04 11:38:20 -0700

    Added logging when requesting ratings from API

--- helpers/rating.py
+++ helpers/rating.py
@@ -9,7 +9,11 @@ def get_rating(apikey,imdbID):
     r = requests.get('http://www.omdbapi.com/?i=' + imdbID + '&apikey=' + apikey)
     log.debug(r)
     if(r.status_code == 200):
+        log.debug("Successfully requested ratings from OMDB")
         for source in json.loads(r.text)["Ratings"]:
             if(source['Source'] == 'Rotten Tomatoes'):
+                log.debug("Rotten Tomatoes shows rating: %s for imdbID: %s".format(source['Value'],imdbID))
                 return int(source['Value'].split('%')[0])
+    else:
+        log.error("ERROR encountered while requesting rating for imdbID: %s Status Code: %d".format(imdbID,r.status_code))
     return -1
